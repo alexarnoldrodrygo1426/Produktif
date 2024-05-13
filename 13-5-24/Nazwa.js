39de642d2dc217e56400d193bb1e16caef2bba2a
alexarnoldrodrygo1426 <168691642+alexarnoldrodrygo1426@users.noreply.github.com>
2024-05-13 14:57:02 +0700

    Create Nazwa.js

---
+++ 13-5-24/Nazwa.js
@@ -0,0 +1,28 @@
+var x="alex";
+    x+=2;
+    console.log(x)
+    
+var x="arnold";
+    x+=6;
+    console.log(x)
+    
+var x="rodrygo";
+    x+=10;
+    console.log(x)
+    
+var x="gabriel";
+    x+=14;
+    console.log(x)
+    
+var x="victoria";
+    x+=26;
+    console.log(x)
+    
+//id//
+
+var y={nama:"nazwa arya",umur:16,absen:17,kelas:"X RPL 3"};
+console.log(y.nama,y.umur,y.absen,y.kelas)
+console.log(y.nama)
+console.log(y.umur)
+console.log(y.absen)
+console.log(y.kelas)
